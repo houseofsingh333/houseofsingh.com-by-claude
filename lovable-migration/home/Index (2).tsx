d29b8a76116e9afa99a3616c663c083951015572
houseofsingh333 <houseofsingh333@gmail.com>
2026-02-16 20:37:05 -0500

    Add files via upload

---
+++ lovable-migration/home/Index (2).tsx
@@ -0,0 +1,23 @@
+import HomeHero from "../sections/HomeHero";
+import HomeIntro from "../sections/HomeIntro";
+import HomeProjects from "../sections/HomeProjects";
+import HomeSpotlight from "../sections/HomeSpotlight";
+import HomeJournal from "../sections/HomeJournal";
+import HomeSpotify from "../sections/HomeSpotify";
+import HomeStudioRedirect from "../sections/HomeStudioRedirect";
+
+const Index = () => {
+  return (
+    <>
+      <HomeHero />
+      <HomeIntro />
+      <HomeProjects />
+      <HomeSpotlight />
+      <HomeJournal />
+      <HomeSpotify />
+      <HomeStudioRedirect />
+    </>
+  );
+};
+
+export default Index;
